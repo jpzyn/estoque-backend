6dcb34be125add14a2d9f534272ede9aa09b4d7d
jpzyn <jpstere25@gmail.com>
2025-11-12 12:54:52 -0300

    model: completar Movimentacao com construtores, accessors e validação

--- src/model/Produto.java
+++ src/model/Produto.java
@@ -3,7 +3,7 @@ public class Produto {
     private double precoUnitario;
     private String unidade;
     private int quantidadeEstoque;
-    private int qunatidadeMinima;
+    private int quantidadeMinima;
     private int quantidadeMaxima;
     private Categoria categoria;
 
@@ -11,5 +11,85 @@ public class Produto {
         this.precoUnitario += this.precoUnitario * (percentual / 100);
     }
 
-    // Construtor, getters e setters
+    public Produto() {
+    }
+
+    public Produto(String nome, double precoUnitario, String unidade, int quantidadeEstoque, int quantidadeMinima, int quantidadeMaxima, Categoria categoria) {
+        this.nome = nome;
+        this.precoUnitario = precoUnitario;
+        this.unidade = unidade;
+        this.quantidadeEstoque = quantidadeEstoque;
+        this.quantidadeMinima = quantidadeMinima;
+        this.quantidadeMaxima = quantidadeMaxima;
+        this.categoria = categoria;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(double precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
+    public String getUnidade() {
+        return unidade;
+    }
+
+    public void setUnidade(String unidade) {
+        this.unidade = unidade;
+    }
+
+    public int getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(int quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public int getQuantidadeMinima() {
+        return quantidadeMinima;
+    }
+
+    public void setQuantidadeMinima(int quantidadeMinima) {
+        this.quantidadeMinima = quantidadeMinima;
+    }
+
+    public int getQuantidadeMaxima() {
+        return quantidadeMaxima;
+    }
+
+    public void setQuantidadeMaxima(int quantidadeMaxima) {
+        this.quantidadeMaxima = quantidadeMaxima;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "nome='" + nome + '\'' +
+                ", precoUnitario=" + precoUnitario +
+                ", unidade='" + unidade + '\'' +
+                ", quantidadeEstoque=" + quantidadeEstoque +
+                ", quantidadeMinima=" + quantidadeMinima +
+                ", quantidadeMaxima=" + quantidadeMaxima +
+                ", categoria=" + categoria +
+                '}';
+    }
 }
